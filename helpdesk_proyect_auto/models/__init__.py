d6a2ff8400a04dc980c0991a2313948901377a45
kcampo@estrasol.com,mx <kcampo@estrasol.com,mx>
2021-05-31 17:48:10 -0500

    a

--- helpdesk_proyect_auto/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-#from .product import product_template
-#from .stock_picking import stock_picking
-#from .sale import sale_order
-#from .res_partner import res_partner
-#from .account_move import account_move
-#from .res_company import res_company
