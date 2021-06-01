d910e81d4955cf9f9fad5d35d68c13f82faaae51
KEVIN DEL CAMPO <kcampo@estrasol.com.mx>
2021-06-01 14:02:36 -0500

    s

--- helpdesk_proyect_auto/__manifest__.py
+++ helpdesk_proyect_auto/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','web','sale'],
+    'depends': ['base','web','sale','sale_timesheet'],
 
     # always loadedd
     'data': [
