2be634f671d98f84b41b50e5ac487f3bb29e52f9
KEVIN DEL CAMPO <kcampo@estrasol.com.mx>
2021-06-02 15:22:01 -0500

    s

--- helpdesk_proyect_auto/models/helpdesk/helpdesk.py
+++ helpdesk_proyect_auto/models/helpdesk/helpdesk.py
@@ -7,7 +7,8 @@ from odoo.exceptions import ValidationError
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
-
+    remaining_status_sale = fields.Selection(related="task_id.sale_line_id.proyect_avaible")
+    scheduled_proyect = fields.Date(related="task_id.sale_line_id.scheduled_proyect")
 
     @api.depends('team_id')
     def _compute_project_id(self):
--- helpdesk_proyect_auto/models/project/project.py
+++ helpdesk_proyect_auto/models/project/project.py
@@ -5,7 +5,7 @@ class ProjectTask(models.Model):
     _inherit = "project.task"
     remaining_status_sale = fields.Selection(related="sale_line_id.proyect_avaible")
     scheduled_proyect = fields.Date(related="sale_line_id.scheduled_proyect")
-    
+    project_stage = fields.Selection([('none','No aplica'),('process','En proceso'),('production','Produccion')],string="Estatus proyecto", tracking=True,) 
             
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
