4320589d6d2dbdf1402055dd27c70dee9e0c8620
KEVIN DEL CAMPO <kcampo@estrasol.com.mx>
2021-06-02 18:41:09 -0500

    project fina

--- helpdesk_proyect_auto/controllers/controllers.py
+++ helpdesk_proyect_auto/controllers/controllers.py
@@ -51,7 +51,7 @@ class WebsiteTicketValidation(http.Controller):
                         
         #items = list(set( [i for i in items] ))                     
         #items_status = list(set( [i for i in items_status] ))
-        if not request.env.user:
+        if  request.env.user:
             return "Hola"
         if items_status:
             return request.render('helpdesk_proyect_auto.mesa_ayuda',{'proys_status':items_status,'user_email':request.env.user.email})
