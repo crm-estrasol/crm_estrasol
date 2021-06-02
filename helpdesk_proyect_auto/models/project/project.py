a4db600fb7217743967697f57ff7bb7ffb86e7f1
KEVIN DEL CAMPO <kcampo@estrasol.com.mx>
2021-06-02 15:25:18 -0500

    s

--- helpdesk_proyect_auto/models/project/project.py
+++ helpdesk_proyect_auto/models/project/project.py
@@ -1,11 +1,13 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-
+class FlexiProject(models.Model):
+    _inherit = "project.project"
+     project_stage = fields.Selection([('none','No aplica'),('process','En proceso'),('production','Produccion')],string="Estatus proyecto", tracking=True,) 
 class ProjectTask(models.Model):
     _inherit = "project.task"
     remaining_status_sale = fields.Selection(related="sale_line_id.proyect_avaible")
     scheduled_proyect = fields.Date(related="sale_line_id.scheduled_proyect")
-    project_stage = fields.Selection([('none','No aplica'),('process','En proceso'),('production','Produccion')],string="Estatus proyecto", tracking=True,) 
+   
             
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
