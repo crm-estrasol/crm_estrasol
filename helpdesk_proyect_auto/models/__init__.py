59ec97fd646437249582a2afd80af17a4d5c1a67
KEVIN DEL CAMPO <kcampo@estrasol.com.mx>
2021-06-09 17:17:39 -0500

    fix bug

--- helpdesk_proyect_auto/models/__init__.py
+++ helpdesk_proyect_auto/models/__init__.py
@@ -3,5 +3,6 @@ from .sale import sale_order
 from .project import project
 
 from .helpdesk import helpdesk
+from .mail import mail
 #from .mrp import mrp_routing
 #from .res_company import res_company
---
+++ helpdesk_proyect_auto/models/mail/mail.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from math import ceil
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+class MailMessageCrm(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model
+    def create(self, values):
+        if  'res_model' in values:
+            if 'helpdesk.ticket' in values['res_model']:
+                values['is_internal'] = True
+        res = super(MailMessageCrm, self).create(values)
+        return res
